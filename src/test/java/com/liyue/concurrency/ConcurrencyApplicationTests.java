9b55c0fe65d149c65ab1b111ae8f8fe47adeba17
菜鸟_1 <602100452@qq.com>
2020-05-11 22:28:50 +0800

    第一次提交

---
+++ src/test/java/com/liyue/concurrency/ConcurrencyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.liyue.concurrency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConcurrencyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
