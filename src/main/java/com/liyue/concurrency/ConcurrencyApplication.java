de7c0b0c68718eeaeb9934082e9e45a213f96dad
菜鸟_1 <602100452@qq.com>
2020-05-13 21:50:24 +0800

    提交

--- src/main/java/com/liyue/concurrency/ConcurrencyApplication.java
+++ src/main/java/com/liyue/concurrency/ConcurrencyApplication.java
@@ -10,4 +10,5 @@ public class ConcurrencyApplication {
 		SpringApplication.run(ConcurrencyApplication.class, args);
 	}
 
+
 }
---
+++ src/main/java/com/liyue/concurrency/annoations/example/count/NotConcurrency.java
@@ -0,0 +1,8 @@
+package com.liyue.concurrency.annoations.example.count;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class NotConcurrency {
+
+}
